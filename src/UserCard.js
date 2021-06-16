fb03323c6b6bcace64a6f403bf2b231d7d03e5cb
theo923 <theotam912@gmail.com>
2021-06-16 21:08:44 +0800

    random image attempt

--- src/UserCard.js
+++ src/UserCard.js
@@ -1,13 +1,16 @@
 import React from 'react'
 
-export const UserCard = ({id, num}) => {
-    if(num[0] > 11) num[0] = 0
-    else num[0]++
+let num = 0;
+let sortCount = 0;
+let sortType = 0;
+
+export const UserCard = ({id}) => {
+    imgSort()
 
     return(
-        <div className={`card shadow-xl image-full ${imageSort(num[0])} `}>
+        <div className={`card shadow-xl image-full ${imgSpan(num)} `}>
             <figure >
-                <img src={imgSrc[num[0]]} alt={id[0]}></img>
+                <img src={imgSrc[num]} alt={id[0]}></img>
             </figure> 
             <div className="justify-end card-body">
                 <h2 className="card-title">{id[0]}</h2> 
@@ -21,23 +24,52 @@ export const UserCard = ({id, num}) => {
     )
 }
 
-const imageSort = (num) => {
-    if((num === 3 || num === 4 || num === 0 || num === 7 || num === 12) && document.documentElement.clientWidth >= 1000) return "col-span-2"
+const imgSpan = (num) => {
+    if((num >= 8) && document.documentElement.clientWidth >= 1000) return "col-span-2 "
+}
+
+const imgSort = () => {
+    debugger
+    if(sortCount === 0) {
+        sortType = Math.floor(Math.random() * 3) + 1;
+        (sortType === 2) ? sortCount = 3 : sortCount = 2;
+    }
+    
+    debugger
+    switch(sortType) {
+        case 1: (sortCount === 3) ? num = Math.floor(Math.random() * 8) : num = Math.floor(Math.random() * 4) + 8
+                sortCount--
+                console.log(`sortType ${sortType}, num ${num}, sortCount ${sortCount}`)
+                break
+        case 2: num = Math.floor(Math.random() * 8)
+                sortCount--
+                console.log(`sortType ${sortType}, num ${num}, sortCount ${sortCount}`)
+                break
+        case 3: (sortCount === 3) ? num = Math.floor(Math.random() * 4) + 8 : num = Math.floor(Math.random() * 8)
+                sortCount--
+                console.log(`sortType ${sortType}, num ${num}, sortCount ${sortCount}`)
+                break
+    }
+    
+    debugger
+    
+    
+    debugger
 }
 
 
 const imgSrc = [
-    'https://na.cx/i/Z9RVOZd.gif',
     'https://na.cx/i/xOgEcjs.gif',
     'https://na.cx/i/YW4JbSJ.gif',
-    'https://na.cx/i/MFQf68K.gif',
-    'https://na.cx/i/A53QCiA.gif',
     'https://na.cx/i/JeO89VK.gif',
     'https://na.cx/i/OPy1Kxy.gif',
-    'https://na.cx/i/zJ00e5e.gif',
     'https://na.cx/i/jdjXhPq.gif',
     'https://na.cx/i/9Dxh1ST.gif',
     'https://na.cx/i/YCgntoK.gif',
     'https://na.cx/i/GuegxQ4.gif',
+    'https://na.cx/i/MFQf68K.gif',
+    'https://na.cx/i/A53QCiA.gif',
+    'https://na.cx/i/zJ00e5e.gif',
+    'https://na.cx/i/Z9RVOZd.gif',
     'https://na.cx/i/VFXFfnA.gif'
 ]
