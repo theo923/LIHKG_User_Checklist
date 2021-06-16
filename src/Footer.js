07cfc1c11b30caa27408e74c4a3ab58b0acb63fe
theo923 <theotam912@gmail.com>
2021-06-17 04:35:47 +0800

    added pointer for link

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
     return(
         <div>
-            <p style={{color: 'blue'}} className='text-center text-base' onClick={() => window.open('https://docs.google.com/spreadsheets/d/1bz9klvGSoiX_oK-i9PM1m1aMBTc8bukQI30NWQcx13Y/edit#gid=289703632', '_blank')}> Data from [LIHKG] ⚠️ on9 list</p>
+            <p style={{color: 'blue'}} className='text-center text-base cursor-pointer' onClick={() => window.open('https://docs.google.com/spreadsheets/d/1bz9klvGSoiX_oK-i9PM1m1aMBTc8bukQI30NWQcx13Y/edit#gid=289703632', '_blank')}> Data from [LIHKG] ⚠️ on9 list</p>
         </div>
     )
 }
