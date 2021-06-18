c0c8fc2fda5b237003e6d92f832160fb6b24f9fb
theo923 <theotam912@gmail.com>
2021-06-19 04:43:53 +0800

    first commit

--- src/Heading.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Heading = () => {
-    return(
-        <div>
-            <p className='text-center text-5xl font-bold border-b-8 lg:text-7xl text-base-content border-primary'>LIHKG User Checklist</p>
-        </div>
-    )
-}
